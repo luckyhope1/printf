99fe985163f2814b6826748b7b30363adfc76527
luckyhope1 <geoffryoduor92@gmail.com>
2023-04-13 15:11:01 -0700

    printf_convo.c

---
+++ printf_convo.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * printf_convo - handle conversion specifier
+ *
+ * Return: 0
+ */
+
+int printf_convo(void)
+{
+	int num = 10;
+
+	printf("The value of num is: %d\n", num);
+
+	int num2 = -5;
+
+	printf("The value of num2 is: %i\n", num2);
+
+	return (0);
+}
+
